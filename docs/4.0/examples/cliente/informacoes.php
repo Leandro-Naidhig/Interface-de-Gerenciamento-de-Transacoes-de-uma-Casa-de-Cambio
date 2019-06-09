5cf838c81b028b35899b272a741dec76b8275dda
Leandro Naidhig <48226434+Leandro-Naidhig@users.noreply.github.com>
2019-06-09 18:06:55 -0300

    Add files via upload

--- docs/4.0/examples/cliente/informacoes.php
+++ docs/4.0/examples/cliente/informacoes.php
@@ -4,19 +4,16 @@
 ?>
 <!doctype html>
 <html lang="pt-br">
-
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-  <link rel="icon" href="icons/favicon.ico">
+  <link rel="icon" href="images/cambio.png">
 
   <title>Informações - UFSBRA Câmbio</title>
 
-  <!-- Custom styles for this template -->
-  <link href="styles/jumbotron_info.css" rel="stylesheet">
-    
+  <link href="styles/jumbotron_info.css" rel="stylesheet">    
   <link href="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
   <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
@@ -24,16 +21,13 @@
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
   <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
-
 <body>
-    
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <img style="height: 50px" src="Imagens/cambio.png" class="img-fluid pr-3">
-    <a class="navbar-brand" href="#">UFSBRA Câmbio</a>
+    <a class="navbar-brand text-white">UFSBRA Câmbio</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item">
@@ -46,7 +40,7 @@
           <a class="nav-link" href="cotacoes.php">Cotações</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="informacoes.php">informações</a>
+          <a class="nav-link active" href="informacoes.php">informações</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="moedas.php">Moedas</a>
@@ -55,8 +49,8 @@
       <?php
         if(isset($_SESSION['usuario_cpf'])) {
         ?>
-            <a href="../cliente/Central_1.html"><button class="btn btn-success my-2 my-sm-0 mr-sm-2" type="submit"><img src="Imagens/central_user.png" width="30px" height="30px"> Seja Bem Vindo <b><?php echo $_SESSION['usuario_nome'] ?></b></button></a>
-            <a href="../cliente/logout.php"><button class="btn btn-danger my-1 my-sm-0" type="submit" style="height:43px;">Sair</button></a>
+            <a href="Central_1.php"><button class="btn btn-success my-2 my-sm-0 mr-sm-2" type="submit"><img src="Imagens/central_user.png" width="30px" height="30px"> Seja Bem Vindo <b><?php echo $_SESSION['usuario_nome'] ?></b></button></a>
+            <a href="logout.php"><button class="btn btn-danger my-1 my-sm-0" type="submit" style="height:43px;">Sair</button></a>
       <?php
         } else {   
        ?>
@@ -69,8 +63,6 @@
   </nav>
 
   <main role="main">
-
-    <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
       <div class="container text-white">
         <h1 class="display-3 text-center">UFSBRA Câmbio</h1>
@@ -78,18 +70,14 @@
       </div>
     </div>
     <div class="flex-container-services">
-
       <h2 class="separation-title"> Como funciona </h2>
       <div class="service-description">Para sua comodidade, comprar moedas agora pode ser feito com um símples clique!</div>
-
-      <!-- start of Web dev container -->
       <div class="flex-item-services">
         <figure class=services-icon>
           <i style="color: #f3701e" class="fa fa-paint-brush" aria-hidden="true"></i>
         </figure>
         <div class="title-services"> Escolha a moeda </div>
         <div class="content-services"> Escolha a moeda a ser comprada e para o ato da compra, valor e modo de entrega. </div>
-
       </div>
       <div class="flex-item-services">
         <figure class=services-icon>
@@ -105,7 +93,6 @@
         <div class="title-services"> Finalize sua compra </div>
         <div class="content-services"> Prontinho! Agora deixa com a gente. </div>
       </div>
-      <!-- end of Web dev container -->
       <div class="container section">
         <div class="row">
           <div class="col-md-6">
@@ -136,7 +123,6 @@
           </div>
         </div>
       </div>
-
       <section class="our-webcoderskull padding-lg">
         <div class="container">
           <div class="row heading heading-icon">
@@ -184,26 +170,13 @@
           </ul>
         </div>
       </section>
-      <!-- end of Social Media container -->
     </div>
-
-
-
   </main>
-
   <footer class="bg-dark">
     <p class="h6 text-center text-light pb-4 pt-4 mb-0">UFSBRA Câmbio - 2019 &copy; Todos os direitos reservados</p>
   </footer>
-
-  <!-- Include jQuery -->
-  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> -->
-
   <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-
-  <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script> -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
 </body>
-
 </html>
--- docs/4.0/examples/cliente/moedas.php
+++ docs/4.0/examples/cliente/moedas.php
@@ -3,37 +3,31 @@
     include('connection.php');
 ?>
 <!doctype html>
-<html lang="en">
-
+<html lang="pt-bt">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-  <link rel="icon" href="images/favicon.ico">
+  <link rel="icon" href="images/cambio.png">
 
   <title>Moedas - UFSBRA Câmbio</title>
 
-  <!-- Custom styles for this template -->
-  <link href="styles/jumbotron.css" rel="stylesheet">
-    
+  <link href="styles/jumbotron.css" rel="stylesheet">    
   <link href="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
   <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
   <link href="http://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
   <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    
 </head>
 <body>
-    
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <img style="height: 50px" src="Imagens/cambio.png" class="img-fluid pr-3">
-    <a class="navbar-brand" href="#">UFSBRA Câmbio</a>
+    <a class="navbar-brand text-white">UFSBRA Câmbio</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item">
@@ -49,14 +43,14 @@
           <a class="nav-link" href="informacoes.php">informações</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="moedas.php">Moedas</a>
+          <a class="nav-link active" href="moedas.php">Moedas</a>
         </li>
       </ul>
       <?php
         if(isset($_SESSION['usuario_cpf'])) {
         ?>
-            <a href="../cliente/Central_1.html"><button class="btn btn-success my-2 my-sm-0 mr-sm-2" type="submit"><img src="Imagens/central_user.png" width="30px" height="30px"> Seja Bem Vindo <b><?php echo $_SESSION['usuario_nome'] ?></b></button></a>
-            <a href="../cliente/logout.php"><button class="btn btn-danger my-1 my-sm-0" type="submit" style="height:43px;">Sair</button></a>
+            <a href="Central_1.php"><button class="btn btn-success my-2 my-sm-0 mr-sm-2" type="submit"><img src="Imagens/central_user.png" width="30px" height="30px"> Seja Bem Vindo <b><?php echo $_SESSION['usuario_nome'] ?></b></button></a>
+            <a href="logout.php"><button class="btn btn-danger my-1 my-sm-0" type="submit" style="height:43px;">Sair</button></a>
       <?php
         } else {   
        ?>
@@ -67,10 +61,7 @@
        ?>
     </div>
   </nav>
-
   <main role="main">
-
-    <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
       <div class="container text-white">
         <h1 class="display-3 text-center">Moedas Estrangeiras</h1>
@@ -81,57 +72,55 @@
       <hr>
       <div class="card-deck">
         <div class="card border-0">
-          <center><img class="card-img-top mt-3" src="Imagens/us-flag.png" style="width:100px"></center>
+          <center><img class="card-img-top mt-3" src="images/us-flag.png" style="width:100px"></center>
           <div class="card-body">
             <h5 class="card-title text-center"><b>USD</b> - Dólar Americano</h5>
             <h6 class="card-title text-center">Estados Unidos</h6>
           </div>
         </div>
         <div class="card border-0">
-          <center><img class="card-img-top rounded-circle" src="Imagens/japan_flag.png"></center>
+          <center><img class="card-img-top rounded-circle" src="images/japan_flag.png"></center>
           <div class="card-body">
             <h5 class="card-title text-center"><b>JPY</b> - Iene</h5>
             <h6 class="card-title text-center">Japão</h6>
           </div>
         </div>
         <div class="card border-0">
-          <center><img class="card-img-top mt-3" src="Imagens/canada_flag.png" alt="Imagem de capa do card" style="width:100px"></center>
+          <center><img class="card-img-top mt-3" src="images/canada_flag.png" style="width:100px"></center>
           <div class="card-body">
             <h5 class="card-title text-center"><b>CAD</b> - Dólar Canadense</h5>
             <h6 class="card-title text-center">Canadá</h6>
           </div>
         </div>
         <div class="card border-0">
-          <center><img class="card-img-top mt-3" src="Imagens/brazil_flag.png" alt="Imagem de capa do card" style="width:100px"></center>
+          <center><img class="card-img-top mt-3" src="images/brazil_flag.png" style="width:100px"></center>
           <div class="card-body">
             <h5 class="card-title text-center"><b>BRL</b> - Real</h5>
             <h6 class="card-title text-center">Brasil</h6>
           </div>
         </div>
         <div class="card border-0">
-          <center><img class="card-img-top mt-3" src="Imagens/britain_flag.png" alt="Imagem de capa do card" style="width:100px"></center>
+          <center><img class="card-img-top mt-3" src="images/britain_flag.png" style="width:100px"></center>
           <div class="card-body">
             <h5 class="card-title text-center"><b>GBP</b> - Libra Esterlina</h5>
+            <h6 class="card-title text-center">Reino Unido</h6>
+          </div>
+        </div>
+        <div class="card border-0">
+          <center><img class="card-img-top mt-3" src="images/britain_flag.png" style="width:100px"></center>
+          <div class="card-body">
+            <h5 class="card-title text-center"><b>EUR</b> - Euro</h5>
             <h6 class="card-title text-center">Reino Unido</h6>
           </div>
         </div>
       </div>
     </div>
   </main>
-
   <footer class="bg-dark">
     <p class="h6 text-center text-light pb-4 pt-4 mb-0">UFSBRA Câmbio - 2019 &copy; Todos os direitos reservados</p>
   </footer>
-
-  <!-- Include jQuery -->
-  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> -->
-
   <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-
-  <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script> -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
 </body>
-
 </html>
