b016e232d4a4f0dad1c81c27307f921d5d57456d
Lucas Andrade <45801831+mrSparroow@users.noreply.github.com>
2019-06-11 03:01:49 -0300

    Delete logout.php

--- cliente/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION['usuario_cpf']);
-    unset($_SESSION['usuario_nome']);
-    session_destroy();
-    header("Location:../cadastro_login/login.php");
-?>
